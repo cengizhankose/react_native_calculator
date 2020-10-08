b7c009df1346b11daa3b392bc89f553ebeb54333
Cengizhan KÖSE <kose651@gmail.com>
2020-10-08 21:04:54 +0300

    styles add

--- src/screens/CalculatorScreen/styles.js
+++ src/screens/CalculatorScreen/styles.js
@@ -5,10 +5,12 @@ export const styles = StyleSheet.create({
   mainContainer: { flex: 1, alignItems: "center" },
   resultsContainer: {
     flex: 1,
-    backgroundColor: "red",
+    backgroundColor: Colors.white,
     width: "100%",
     justifyContent: "flex-end",
     alignItems: "flex-end",
+    paddingRight: "10%",
+    paddingBottom: "3%",
   },
   buttonsContainer: {
     flex: 1.5,
@@ -16,4 +18,21 @@ export const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  innerButtonsContainer: { flex: 1, marginVertical: "10%" },
+  buttonLightGray: { color: Colors.lightGray },
+  buttonGray: { color: Colors.gray },
+  buttonRed: { color: Colors.red },
+  buttonEqual: {
+    backgroundColor: Colors.red,
+    shadowColor: Colors.red,
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+
+    elevation: 12,
+  },
+  seperator: { width: "80%", height: 1, backgroundColor: Colors.seperator },
 });
