3b86ca0a9e21008dbb4f2c9032abcf06e7ba5a3f
Cengizhan KÖSE <kose651@gmail.com>
2020-10-08 17:29:32 +0300

    Main screen add

--- App.js
+++ App.js
@@ -1,21 +1,6 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import CalculatorScreen from "./src/screens/CalculatorScreen/index";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your !</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <CalculatorScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
