6e5cdf1efa2787fb4d99505a72e2ebe1bae5fe2f
Cengizhan KÖSE <kose651@gmail.com>
2020-10-10 22:28:36 +0300

    redux store to app add

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
 import CalculatorScreen from "./src/screens/CalculatorScreen/index";
 
+import thunk from "redux-thunk";
+import reducer from "./src/redux/index";
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
 export default function App() {
-  return <CalculatorScreen />;
+  return (
+    <Provider store={store}>
+      <CalculatorScreen />
+    </Provider>
+  );
 }
