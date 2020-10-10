c26fc7fa99fff709a2af39e7e1dd438d784553ec
Cengizhan KÖSE <kose651@gmail.com>
2020-10-10 22:29:31 +0300

    Logic to UI Components update

--- src/screens/CalculatorScreen/index.js
+++ src/screens/CalculatorScreen/index.js
@@ -1,59 +1,185 @@
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import { RFPercentage } from "react-native-responsive-fontsize";
+import { connect } from "react-redux";
+//styling
+import { Colors } from "../../constants/Colors/Colors";
 import { styles } from "./styles";
-
+//components
 import Row from "../../components/Row";
 import Button from "../../components/Button";
+//actions
+import {
+  updateCurrentValue,
+  handleEqual,
+  handleClear,
+  handleNegative,
+  handlePercentage,
+} from "../../redux/actions";
 
-import { Colors } from "../../constants/Colors/Colors";
-
-export default function CalculatorScreen() {
+function CalculatorScreen(props) {
   return (
     <SafeAreaView style={styles.mainContainer}>
       <View style={styles.resultsContainer}>
         <Text style={{ fontSize: RFPercentage(4), color: Colors.lightGray }}>
-          2*50*3
+          {props.previousValue}
         </Text>
         <Text style={{ fontSize: RFPercentage(12), color: Colors.gray }}>
-          300
+          {props.currentValue}
         </Text>
       </View>
       <View style={styles.seperator} />
       <View style={styles.buttonsContainer}>
         <View style={styles.innerButtonsContainer}>
           <Row>
-            <Button textStyle={styles.buttonGray} value="C" />
-            <Button textStyle={styles.buttonGray} value="+/-" />
-            <Button textStyle={styles.buttonGray} value="%" />
-            <Button textStyle={styles.buttonRed} value="/" />
+            <Button
+              textStyle={styles.buttonGray}
+              value="C"
+              onPress={() => {
+                props.handleClear();
+              }}
+            />
+            <Button
+              textStyle={styles.buttonGray}
+              value="+/-"
+              onPress={() => {
+                props.handleNegative(props.currentValue);
+              }}
+            />
+            <Button
+              textStyle={styles.buttonGray}
+              value="%"
+              onPress={() => {
+                props.handlePercentage(props.currentValue);
+              }}
+            />
+            <Button
+              textStyle={styles.buttonRed}
+              value="/"
+              onPress={() => {
+                props.updateCurrentValue("/", props.currentValue);
+              }}
+            />
           </Row>
           <Row>
-            <Button textStyle={styles.buttonLightGray} value="7" />
-            <Button textStyle={styles.buttonLightGray} value="8" />
-            <Button textStyle={styles.buttonLightGray} value="9" />
-            <Button textStyle={styles.buttonRed} value="x" />
+            <Button
+              textStyle={styles.buttonLightGray}
+              value="7"
+              onPress={() => {
+                props.updateCurrentValue("7", props.currentValue);
+              }}
+            />
+            <Button
+              textStyle={styles.buttonLightGray}
+              value="8"
+              onPress={() => {
+                props.updateCurrentValue("8", props.currentValue);
+              }}
+            />
+            <Button
+              textStyle={styles.buttonLightGray}
+              value="9"
+              onPress={() => {
+                props.updateCurrentValue("9", props.currentValue);
+              }}
+            />
+            <Button
+              textStyle={styles.buttonRed}
+              value="x"
+              onPress={() => {
+                props.updateCurrentValue("*", props.currentValue);
+              }}
+            />
           </Row>
           <Row>
-            <Button textStyle={styles.buttonLightGray} value="4" />
-            <Button textStyle={styles.buttonLightGray} value="5" />
-            <Button textStyle={styles.buttonLightGray} value="6" />
-            <Button textStyle={styles.buttonRed} value="-" />
+            <Button
+              textStyle={styles.buttonLightGray}
+              value="4"
+              onPress={() => {
+                props.updateCurrentValue("4", props.currentValue);
+              }}
+            />
+            <Button
+              textStyle={styles.buttonLightGray}
+              value="5"
+              onPress={() => {
+                props.updateCurrentValue("5", props.currentValue);
+              }}
+            />
+            <Button
+              textStyle={styles.buttonLightGray}
+              value="6"
+              onPress={() => {
+                props.updateCurrentValue("6", props.currentValue);
+              }}
+            />
+            <Button
+              textStyle={styles.buttonRed}
+              value="-"
+              onPress={() => {
+                props.updateCurrentValue("-", props.currentValue);
+              }}
+            />
           </Row>
           <Row>
-            <Button textStyle={styles.buttonLightGray} value="1" />
-            <Button textStyle={styles.buttonLightGray} value="2" />
-            <Button textStyle={styles.buttonLightGray} value="3" />
-            <Button textStyle={styles.buttonRed} value="+" />
+            <Button
+              textStyle={styles.buttonLightGray}
+              value="1"
+              onPress={() => {
+                props.updateCurrentValue("1", props.currentValue);
+              }}
+            />
+            <Button
+              textStyle={styles.buttonLightGray}
+              value="2"
+              onPress={() => {
+                props.updateCurrentValue("2", props.currentValue);
+              }}
+            />
+            <Button
+              textStyle={styles.buttonLightGray}
+              value="3"
+              onPress={() => {
+                props.updateCurrentValue("3", props.currentValue);
+              }}
+            />
+            <Button
+              textStyle={styles.buttonRed}
+              value="+"
+              onPress={() => {
+                props.updateCurrentValue("+", props.currentValue);
+              }}
+            />
           </Row>
           <Row>
-            <Button textStyle={styles.buttonLightGray} value="00" />
-            <Button textStyle={styles.buttonLightGray} value="0" />
-            <Button value="." />
+            <Button
+              textStyle={styles.buttonLightGray}
+              value="00"
+              onPress={() => {
+                props.updateCurrentValue("00");
+              }}
+            />
+            <Button
+              textStyle={styles.buttonLightGray}
+              value="0"
+              onPress={() => {
+                props.updateCurrentValue("0", props.currentValue);
+              }}
+            />
+            <Button
+              value="."
+              textStyle={styles.buttonGray}
+              onPress={() => {
+                props.updateCurrentValue(".");
+              }}
+            />
             <Button
               style={styles.buttonEqual}
               textStyle={{ color: Colors.white, fontWeight: "bold" }}
               value="="
+              onPress={() => {
+                props.handleEqual(props.currentValue);
+              }}
             />
           </Row>
         </View>
@@ -61,3 +187,16 @@ export default function CalculatorScreen() {
     </SafeAreaView>
   );
 }
+
+const mapStateToProps = ({ reducer }) => {
+  const { currentValue, previousValue } = reducer;
+  return { currentValue, previousValue };
+};
+
+export default connect(mapStateToProps, {
+  updateCurrentValue,
+  handleEqual,
+  handleClear,
+  handleNegative,
+  handlePercentage,
+})(CalculatorScreen);
